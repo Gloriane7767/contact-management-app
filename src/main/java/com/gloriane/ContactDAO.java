cd47282a120a999e759d76aaad039cfe1a3cebd6
Glory Ngum Foncha <gloriane7767@gmail.com>
2025-12-10 13:03:27 +0100

    implement add new contact to contact dao

--- src/main/java/com/gloriane/ContactDAO.java
+++ src/main/java/com/gloriane/ContactDAO.java
@@ -1,6 +1,36 @@
 package com.gloriane;
 
-// Data Access & logic Object for  store and managing contacts
+// Data Access & logic Object for storing and managing contacts
 public class ContactDAO {
 
+    // This array will store up to 100 contacts in the format "name|mobile"
+    static String[] contacts = new String[5];
+
+    // This keeps track of how many contacts we have saved
+    static int size = 0;
+
+    // ---------------- ADD CONTACT ----------------
+    public  static boolean addContact(String name, String phoneNumber) {
+        String newContact = name + "|" + phoneNumber;
+
+        if (name.isEmpty() || phoneNumber.isEmpty()) {
+            System.out.println("Name or mobile number cannot be empty.");
+            return false;
+            }
+        if (size >= contacts.length) {
+            System.out.println("Contact list is full. Cannot add more contacts.");
+            return false;
+        }
+        contacts[size] = name + "|" + phoneNumber;
+        size++;
+        return true;
+    }
+
+    public static void searchByName(String name) {
+        // TODO: Implement search functionality
+    }
+
+    public static void displayAllContacts() {
+        // TODO: Implement display functionality
+    }
 }
